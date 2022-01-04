0dcf63d65a6ca9be72371938dc2ed5001f4228f1
ImrangirHossain <imrangir.920@gmail.com>
2022-01-05 00:25:25 +0600

    slider added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Pages/Footer/Footer";
 import Header from "./Pages/Footer/Header/Header";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <Router>
       <Header />
+      <Home/>
       <Routes>
-        <Route></Route>
+        <Route>
+        </Route>
       </Routes>
       <Footer />
     </Router>
---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import banner1 from '../../images/banner-slider.jpg'
+import banner2 from '../../images/banner-slider-2.jpg'
+import banner3 from '../../images/banner-slider-3.jpg'
+import sliderText from '../../images/slider-text-2.png'
+import sliderText3 from '../../images/slider-text.png'
+import sliderText2 from '../../images/slider-text-3.png'
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="slider-1 carousel-item active">
+                    <img height="450px" className="d-block w-100" src={banner3} alt="third slide"/>
+                <div className="carousel-caption text-center text-black d-block ">
+                <img className="w-50" src={sliderText3} alt="" />
+                    <p className="text-white fw-bold mb-2"><i>the wooboom clothing summer collection is back at half price</i></p>
+                    <Link to="/explore">
+                         <button className="btn btn-outline-light my-3 mb-5">Discover Now</button>
+                    </Link>
+                </div>
+              
+                </div>
+                <div className="slider-2 carousel-item">
+                <img height="450px" className="d-block w-100" src={banner2} alt="Second slide"/>
+                <div className="carousel-caption  text-center text-black d-block col-md-4 col-6">
+                <img className="w-100" src={sliderText2} alt="" />
+            
+                    <p className="text-muted fw-bold mb-2">the wooboom spring collection is back at half price. </p>
+                    <Link to="/explore">
+                         <button className="btn btn-outline-light my-3 mb-5">Discover Now</button>
+                    </Link>
+                </div>
+                </div>
+                <div className="slider-3 carousel-item">
+                <img height="450px" className="d-block w-100" src={banner1} alt="First slide"/>
+                <div className="carousel-caption  text-center text-black d-block col-md-4 col-6">
+                <img className="w-100" src={sliderText} alt="" />
+                    <p className="text-white fw-bold mb-2">the wooboom jacket collection is back at half price. </p>
+                    <Link to="/explore">
+                         <button className="btn btn-outline-light my-3 mb-5">Discover Now</button>
+                    </Link>
+
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Banner;
+
